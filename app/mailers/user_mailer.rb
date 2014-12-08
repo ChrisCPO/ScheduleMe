f4ab5a0a917fb04dc185e58a06e4e49ceb40fa11
ChrisCPO <ChrisCPO@gmail.com>
2014-12-08 15:06:16 -0500

    small edits to form and email
    * added errors and
    * link is sent in email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: "ScheduleME@example.com"
 
   def mail_login_info(user, password)
+    @user = user
     @password = password
-    mail(to: user.email, subject: "Your New Account On ScheduleMe")
+    mail(to: @user.email, subject: "Your New Account On ScheduleMe")
   end
 end

d3adaeaa3ad9a2e899d5c7faab4a8afb55402872
ChrisCPO <ChrisCPO@gmail.com>
2014-12-09 14:16:16 -0500

    so far added role to users and ROLE.enum in users.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,23 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true
+  validates :role, presence: true
 
   has_one :password_reset
 
   belongs_to :location
 
+  ROLE = ["employee", "manager"].to_enum
+
   def self.new_password
     { password: SecureRandom.hex(8) }
+  end
+
+  def default_role
+    ROLE.first
+  end
+
+  def Set_manager
+    ROLE.last
   end
 end

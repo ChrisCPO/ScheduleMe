9b02961f0ec8a147a381a6f8fcd549d55c82aed2
ChrisCPO <ChrisCPO@gmail.com>
2014-12-15 11:54:56 -0500

    forgot to save a file

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,5 @@
 class LocationsController < ApplicationController
-  before_action :user_no_location, only: [:create, :new]
+  before_action :user_has_no_location, only: [:create, :new]
   def new
     @location = Location.new
   end

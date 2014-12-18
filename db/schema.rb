15c8c8a80b3977780a613afb22221aa4414f6317
ChrisCPO <ChrisCPO@gmail.com>
2014-12-18 10:42:58 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215225242) do
+ActiveRecord::Schema.define(version: 20141215222003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,15 +36,6 @@ ActiveRecord::Schema.define(version: 20141215225242) do
 
   create_table "schedules", force: true do |t|
     t.integer  "location_id", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "shifts", force: true do |t|
-    t.integer  "user_id",    null: false
-    t.date     "shift_date", null: false
-    t.integer  "shift_time"
-    t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

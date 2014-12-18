bfaefdb8f72f6fc4c63f7780930788b26b72d0b9
ChrisCPO <ChrisCPO@gmail.com>
2014-12-18 10:41:49 -0500

    added index to user_id

--- db/migrate/20141215225242_create_shifts.rb
+++ db/migrate/20141215225242_create_shifts.rb
@@ -1,7 +1,7 @@
 class CreateShifts < ActiveRecord::Migration
   def change
     create_table :shifts do |t|
-      t.references :user, null: false
+      t.references :user, null: false, index: true
       t.date :shift_date, null: false
       t.integer :shift_time
       t.text :notes
